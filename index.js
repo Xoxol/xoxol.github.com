6907c1a7db4e33bd2d4c45bc51cc7159197e4caf
Evgeniy Kulikov <e.kulikov@it-online.ru>
2014-08-06 19:04:24 +0400

    Изменен сервер с http на connect

--- index.js
+++ index.js
@@ -1,67 +1,31 @@
 /**
  * Created by e.kulikov on 04.08.2014.
  */
-var http = require('http'),
+var connect = require('connect'),
+    dispatch = require('dispatch'),
     less = require('less'),
-    url = require('url'),
     fs = require('fs'),
-    parser = new less.Parser,
-    server = new http.Server(),
-    emit = server.emit,
-    options = {
-        'css': '',
-        'dev': {},
-        'min': {
-            'compress': true
-        }
-    };
+    config = require('./styles/configuration');
 
-function render(res) {
-    return function(err, lessString) {
+connect()
+    .use(dispatch(
+        {
+            '.+\\.css(\\?.*)?': lessCompiler
+        }
+    ))
+    .listen(1337);
+
+function lessCompiler (req, res, next) {
+    var file = req._parsedUrl.pathname.replace('.css', '.less');
+
+    fs.readFile('.' + file, 'utf-8', go);
+
+    function go (err, code) {
         if(err) {
-            console.log(err);
-            return;
+            console.log(err.message); //ToDo: заменить console.log на нормальный логгер
+            res.end(err.message);
         }
 
-        parser.parse(lessString, function(err, tree) {
-
-        });
+        res.end(code);
     }
 }
-
-function compile (res, option) {
-    return function (err, lessString) {
-        if(err) {
-            console.log(err);
-            return;
-        }
-
-        parser.parse(lessString.toString(), function(err, tree) {
-            if(err) {
-                console.log(err);
-                res.end(err.toString());
-                return;
-            }
-
-            console.log(tree);
-            res.end(tree.toCSS(options[option]));
-        });
-    }
-}
-
-function request (req, res) {
-    var path = url.parse(req.url).path,
-        file = path.split('.');
-
-    fs.readFile('.' + file[0] + '.less', compile(res, file[1]));
-
-}
-
-server.emit = function(e) {
-    console.log(e);
-    emit.apply(server, arguments);
-};
-
-server.listen(1337, '127.0.0.1');
-
-server.on('request', request);
